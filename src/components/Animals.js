3a29611ea8eea5c2d4fd286f0a9a99ba9474a007
Jesse Gould <jesse.gould@codeimmersives.com>
2020-04-09 11:25:21 -0400

    first commit

---
+++ src/components/Animals.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+
+let images = [
+    
+  'images/image1.jpeg',
+  'images/image2.jpeg',
+  'images/image3.jpeg',
+  'images/image4.jpeg',
+
+  ];
+
+
+  class Animals extends Component{
+    state = {
+      images,
+  };
+
+  
+    render(){
+      return(
+        <div>
+            Test222
+        </div>
+      )
+    }
+  }
+
+
+
+
+  export default Animals;
