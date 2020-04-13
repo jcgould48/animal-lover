4210c55fe562c0df991426c8241eb5cce9b70a7b
Jesse Gould <jesse.gould@codeimmersives.com>
2020-04-13 07:14:51 -0400

    adds functionality of sidecard

--- src/components/Animals.js
+++ src/components/Animals.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import Sidecard from './SideCard';
+import './Animals.css';
 
 let animals = [
     {
@@ -7,7 +9,6 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/image1.jpeg',
         animalId: '1',
-        liked: '',
       },
       {
         name: 'Tim',
@@ -15,7 +16,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/image2.jpeg',
         animalId: '2',
-        liked: '',
+   
       },
       {
         name: 'Larry',
@@ -23,7 +24,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/image3.jpeg',
         animalId: '3',
-        liked: '',
+     
       }, 
       {
         name: 'Sam',
@@ -31,7 +32,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/image4.jpeg',
         animalId: '4',
-        liked: '',
+     
       },
       {
         name: 'Mary',
@@ -39,7 +40,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/dog1.jpeg',
         animalId: '5',
-        liked: '',
+ 
       },
       {
         name: 'Tim',
@@ -47,7 +48,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/dog2.jpeg',
         animalId: '6',
-        liked: '',
+    
       },
       {
         name: 'Larry',
@@ -55,7 +56,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/dog3.jpeg',
         animalId: '7',
-        liked: '',
+     
       }, 
       {
         name: 'Sam',
@@ -63,7 +64,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/dog4.jpeg',
         animalId: '8',
-        liked: '',
+     
       },
       {
         name: 'Mary',
@@ -71,7 +72,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/cat1.jpeg',
         animalId: '9',
-        liked: '',
+    
       },
       {
         name: 'Tim',
@@ -79,7 +80,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/cat2.jpeg',
         animalId: '10',
-        liked: '',
+      
       },
       {
         name: 'Larry',
@@ -87,7 +88,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/cat3.jpeg',
         animalId: '11',
-        liked: '',
+       
       }, 
       {
         name: 'Sam',
@@ -95,7 +96,7 @@ let animals = [
         description: 'Parrot with wings',
         image: 'images/cat4.jpeg',
         animalId: '12',
-        liked: '',
+        
       },
 
   ];
@@ -106,7 +107,9 @@ let animals = [
     super()
     this.state = {
         animals,
-        liked: '',
+        liked:[],
+        disliked:[],
+        sidebar: '',
     };
     this.onDelete = this.onDelete.bind(this)
 }
@@ -118,57 +121,74 @@ onDelete(id){
         console.log('Delete item with id: ', id);
 }
 
-onLike = (event) => {
-    this.setState({liked: 'yes'}, ()=>{console.log(this.state.animals)})
+onLike = (animalId) => {
+    const likedAnimal = this.state.animals.filter(animal=> animal.animalId === animalId)[0]
+    const liked = [...this.state.liked]
+    console.log(liked)
+    if(!liked.includes(likedAnimal)){
+       if(!this.state.disliked.includes(likedAnimal)){
+          liked.push(likedAnimal)
+       }
+    }
+    this.setState({liked})
+ };
     
+
+onDislike = (animalId) => {
+        const dislikedAnimal = this.state.animals.filter(animal=> animal.animalId === animalId)[0]
+        const disliked = [...this.state.disliked]
+        console.log(disliked)
+        if(!disliked.includes(dislikedAnimal)){
+           if(!this.state.liked.includes(dislikedAnimal)){
+           disliked.push(dislikedAnimal)
+           }
+        }
+        this.setState({disliked})
 };
 
-onDislike = (id) => {
-    const updatedAnimal = this.state.animals.filter((item) => {
-        if(item.animalId === id){
-            this.setState({liked: 'no'})
-        }
-    });
+deleteLiked = (id) => {
+  const deletedAnimal = [...this.state.likes].filter(({animalId}) => animalId !== id);
+  this.setState({ liked: deletedAnimal });
+};
 
-    
-    console.log('Delete item with id: ', id);
+deleteDisliked = (id) => {
+  const deletedAnimal = [...this.state.dislikes].filter(({animalId}) => animalId !== id);
+  this.setState({ disliked: deletedAnimal });
 };
 
 
     render(){
       return(
-        <div>
+        <div style={{display: 'inlineBlock'}}>
     {this.state.animals
 .map(({image, type, name, description, animalId}) =>{
     return(
-        <div class="ui four column grid">
-  <div class="column">
-    <div class="ui fluid card">
-      <div class="image">
-        <img src={image}/>
+        <div className="ui six column grid">
+  <div className="column">
+    <div className="ui fluid card">
+      <div className="image">
+        <img src={image} alt="..."/>
       </div>
-      <div class="content">
-          <a class="header">{name}</a>
-          <div class="meta">{type}</div>
-      <div class="description">
+      <div className="content">
+          <a className="header">{name}</a>
+          <div className="meta">{type}</div>
+      <div className="description">
       {description}
       </div>
         </div>
-        <div class="extra content">
+        <div className="extra content">
           <a>
           <button 
         className="ui blue button" 
         style={{margin:'10px 15px'}} 
-        onClick={this.onLike}
+        onClick={() => this.onLike(animalId)}
        >
         Like
        </button>
        <button 
         className="ui red button" 
         style={{margin:'10px 15px'}} 
-        onClick={() =>{
-            return this.onDislike(animalId)
-        }}
+        onClick={() => this.onDislike(animalId)}
        >
         Dislike
        </button>
@@ -188,6 +208,35 @@ onDislike = (id) => {
   </div>
     )
 })} 
+<div className='side'>
+    <div>
+        <h3>Likes</h3>
+        <ul style={{listStyleType: 'none'}}>
+            {this.state.liked.map(({image, name, animalId}) => {
+                return (
+                <div className='sidecard' onClick={() => this.deleteLiked(animalId)}>
+                    <img src={image} alt="..." />
+                    <p>{name}</p>
+                </div>
+                    
+                )
+            })}
+        </ul>
+    </div>
+    <div>
+        <h3>Dislikes</h3>
+        <ul style={{listStyleType: 'none'}}>
+        {this.state.disliked.map(({image, name, animalId}) => {
+                return (
+                <div className='sidecard' onClick={() => this.deleteDisliked(animalId)}>
+                    <img src={image} alt="..." />
+                    <p>{name}</p>
+                </div>
+                )
+            })}
+        </ul>
+    </div>
+</div>
         </div>
       )
     }
